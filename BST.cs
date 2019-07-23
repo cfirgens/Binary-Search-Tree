66a47901ced78b51adf368eb8f809c3e933ec016
cfirgens <firgenscj@gmail.com>
2019-07-23 11:36:21 -0500

    added in add method for BST, untested

--- BST.cs
+++ BST.cs
@@ -8,5 +8,46 @@ namespace Binary_Search_Tree
 {
     class BST
     {
+        //member variables
+        Node root = null;
+        //Constructor
+
+
+        public void Add(int item)
+        {
+            Node node = new Node(item);
+
+            if (root == null)
+            {
+                root = node;
+                return;
+            }             
+
+            else
+            {
+                Node parent = root;
+            
+                if (item > parent.data)
+                {
+                    if (parent.right == null)
+                    {
+                        parent.right = new Node(item);
+                    }
+
+                }
+                else
+                {
+                    if (parent.left == null)
+                    {
+                        parent.left = new Node(item);
+                    }
+
+                }
+            }
+        }
+
+        
+
+
     }
 }
